7c0c2d0a3ada07df751d4d12b1856f3ad14bed70
jalvathi07 <138186019+jalvathi07@users.noreply.github.com>
2023-06-30 10:19:42 +0530

    Add files via upload

---
+++ TEA STALL/TEA STALL/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace TEA_STALL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(263, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CHAYA CORNER";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(179, 131);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(89, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(292, 130);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(88, 21);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(417, 130);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(89, 21);
+            this.comboBox3.TabIndex = 6;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(179, 88);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(67, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "DRINKS";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(292, 88);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(96, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "COOLDRINKS";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(417, 88);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(69, 17);
+            this.checkBox3.TabIndex = 9;
+            this.checkBox3.Text = "SNACKS";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "ITEMS :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "VARITIES :";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(179, 172);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown1.TabIndex = 12;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(292, 172);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(44, 20);
+            this.numericUpDown2.TabIndex = 13;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(417, 172);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(41, 20);
+            this.numericUpDown3.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(101, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "COUNT :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 30);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "B I L L";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(216, 224);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(242, 121);
+            this.listBox1.TabIndex = 17;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(90, 298);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 25);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "C L E A R";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 375);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TEA STALL/TEA STALL/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TEA_STALL
+{
+    public partial class Form1 : Form
+    {
+        public static int bill = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("TEA");
+            comboBox1.Items.Add("COFFEE");
+            comboBox1.Items.Add("LEMON TEA");
+            comboBox2.Items.Add("SODA");
+            comboBox2.Items.Add("JUICE");
+            comboBox2.Items.Add("SHAKES");
+            comboBox3.Items.Add("VADA");
+            comboBox3.Items.Add("BISCUITS");
+            comboBox3.Items.Add("PUFFS");
+            listBox1.Items.Add("                        CHAYA CORNER");
+            listBox1.Items.Add("");
+            listBox1.Items.Add("                               B I L L");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                comboBox1.Enabled = true;
+                numericUpDown1.Enabled = true;
+                button1.Enabled = true;
+            }
+            else
+            {
+                comboBox1.Enabled = false;
+                comboBox1.SelectedIndex = -1;
+                numericUpDown1.Enabled = false;
+                numericUpDown1.Value = 0;
+                button1.Enabled = false;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                comboBox2.Enabled = true;
+                numericUpDown2.Enabled = true;
+                button1.Enabled = true;
+            }
+            else
+            {
+                comboBox2.Enabled = false;
+                comboBox2.SelectedIndex = -1;
+                numericUpDown2.Enabled = false;
+                numericUpDown2.Value = 0;
+                button1.Enabled = false;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                comboBox3.Enabled = true;
+                numericUpDown3.Enabled = true;
+                button1.Enabled = true;
+            }
+            else
+            {
+                comboBox3.Enabled = false;
+                comboBox3.SelectedIndex = -1;
+                numericUpDown3.Enabled = false;
+                numericUpDown3.Value = 0;
+                button1.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int drinks = 0;
+            int cooldrinks = 0;
+            int snacks = 0;
+            if (checkBox1.Checked)
+            {
+                if (comboBox1.Text == "TEA")
+                {
+                    int tea = 10;
+                    drinks = (int)numericUpDown1.Value * tea;
+                    listBox1.Items.Add("COST FOR TEA :" + drinks);
+                    bill = bill + drinks;
+                }
+                else if (comboBox1.Text == "LEMON TEA")
+                {
+                    int tea = 15;
+                    drinks = (int)numericUpDown1.Value * tea;
+                    listBox1.Items.Add("COST FOR  LEMON TEA :" + drinks);
+                    bill = bill + drinks;
+                }
+                else if (comboBox1.Text == "COFFEE")
+                {
+                    int tea = 15;
+                    drinks = (int)numericUpDown1.Value * tea;
+                    listBox1.Items.Add("COST FOR COFFEE :" + drinks);
+                    bill = bill + drinks;
+                }
+                if (checkBox2.Checked)
+                {
+                    if (comboBox2.Text == "SODA")
+                    {
+                        int soda = 20;
+                        cooldrinks = (int)numericUpDown2.Value * soda;
+                        listBox1.Items.Add("COST FOR SODA :" + cooldrinks);
+                        bill = bill + cooldrinks;
+                    }
+                    else if (comboBox2.Text == "SHAKES")
+                    {
+                        int shakes = 30;
+                        cooldrinks = (int)numericUpDown2.Value * shakes;
+                        listBox1.Items.Add("COST FOR SODA :" + cooldrinks);
+                        bill = bill + cooldrinks;
+                    }
+                    else if (comboBox2.Text == "JUICE")
+                    {
+                        int juice = 25;
+                        cooldrinks = (int)numericUpDown2.Value * juice;
+                        listBox1.Items.Add("COST FOR JUICE :" + cooldrinks);
+                        bill = bill + cooldrinks;
+                    }
+                    if (checkBox3.Checked)
+                    {
+                        if (comboBox3.Text == "VADA")
+                        {
+                            int vada = 8;
+                            snacks = (int)numericUpDown3.Value * vada;
+                            listBox1.Items.Add("COST FOR VADA :" + cooldrinks);
+                            bill = bill + snacks;
+                        }
+                        else if (comboBox3.Text == "BISCUITS")
+                        {
+                            int biscuits = 5;
+                            snacks = (int)numericUpDown3.Value * biscuits;
+                            listBox1.Items.Add("COST FOR VADA :" + snacks);
+                            bill = bill + snacks;
+                        }
+                        else if (comboBox3.Text == "PUFFS")
+                        {
+                            int puffs = 20;
+                            snacks = (int)numericUpDown3.Value * puffs;
+                            listBox1.Items.Add("COST FOR PUFFS :" + snacks);
+                            bill = bill + snacks;
+                        }
+                    }
+                    listBox1.Items.Add(" TOTAL AMOUNT :" + bill);
+                }
+            }
+        }
+           
+            
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                            listBox1.Items.Clear();
+                            checkBox1.Checked = false;
+                            checkBox2.Checked = false;
+                            checkBox3.Checked = false;
+                            comboBox1.SelectedIndex = -1;
+                            comboBox2.SelectedIndex = -1;
+                            comboBox3.SelectedIndex = -1;
+                            comboBox1.Enabled = false;
+                            comboBox2.Enabled = false;
+                            comboBox3.Enabled = false;
+                            numericUpDown1.Enabled = false;
+                            numericUpDown2.Enabled = false;
+                            numericUpDown3.Enabled = false;
+                            numericUpDown1.Value = 0;
+                            numericUpDown2.Value = 0;
+                            numericUpDown3.Value = 0;
+        }
+       
+         private void button3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Focus();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            numericUpDown2.Focus();
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            numericUpDown3.Focus();
+        }
+    }
+}
+
+
